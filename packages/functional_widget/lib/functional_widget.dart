0e2339729243a32f8f160df6eeb60e36008fd334
Remi Rousselet <darky12s@gmail.com>
2018-12-02 17:39:21 +0100

    implementation done

---
+++ packages/functional_widget/lib/functional_widget.dart
@@ -0,0 +1,9 @@
+class _FunctionalWidget {
+  const _FunctionalWidget();
+}
+
+/// A decorator for functions to generate a widget from their prototype.
+/// The name of the generated widget is the name of the decorated function,
+/// with an uppercase as first letter.
+const widget = _FunctionalWidget();
+

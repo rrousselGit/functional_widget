56856d16ec8df8087b1ef95add9e3ba10bc43c29
Remi Rousselet <darky12s@gmail.com>
2018-12-03 00:24:28 +0100

    ci

--- tool/travis.sh
+++ tool/travis.sh
@@ -19,10 +19,15 @@ EXIT_CODE=0
 while (( "$#" )); do
   TASK=$1
   case $TASK in
-  dartanalyzer) echo
-    echo -e '\033[1mTASK: dartanalyzer\033[22m'
+  dartanalyzer_0) echo
+    echo -e '\033[1mTASK: dartanalyzer_0\033[22m'
     echo -e 'dartanalyzer --fatal-infos --fatal-warnings .'
     dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
+    ;;
+  dartanalyzer_1) echo
+    echo -e '\033[1mTASK: dartanalyzer_1\033[22m'
+    echo -e 'dartanalyzer --fatal-infos --fatal-warnings lib test'
+    dartanalyzer --fatal-infos --fatal-warnings lib test || EXIT_CODE=$?
     ;;
   dartfmt) echo
     echo -e '\033[1mTASK: dartfmt\033[22m'

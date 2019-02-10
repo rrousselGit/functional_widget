974b932b12d6cc0dbfffe5e82f5e73515f7872ec
Remi Rousselet <darky12s@gmail.com>
2019-02-10 07:05:00 +0100

    upgrade packages

--- functional_widget_annotation/lib/functional_widget_annotation.dart
+++ functional_widget_annotation/lib/functional_widget_annotation.dart
@@ -1,12 +1,21 @@
-class _FunctionalWidget {
-  const _FunctionalWidget();
+enum FunctionalWidgetType {
+  hook,
+  stateless,
+}
+
+class FunctionalWidget {
+  const FunctionalWidget({this.type});
+
+  final FunctionalWidgetType type;
 }
 
 /// A decorator for functions to generate a `StatelessWidget`.
 ///
 /// The name of the generated widget is the name of the decorated function,
 /// with an uppercase as first letter.
-const widget = _FunctionalWidget();
+const FunctionalWidget widget = FunctionalWidget(
+  type: FunctionalWidgetType.stateless,
+);
 
 /// A decorator for functions to generate a `HookWidget`.
 ///
@@ -18,4 +27,6 @@ const widget = _FunctionalWidget();
 ///
 /// The name of the generated widget is the name of the decorated function,
 /// with an uppercase as first letter.
-const hwidget = _FunctionalWidget();
+const FunctionalWidget hwidget = FunctionalWidget(
+  type: FunctionalWidgetType.hook,
+);

401af08e1d5239e5c2bd7694bcb6f352ec2f2e98
Remi Rousselet <darky12s@gmail.com>
2019-02-13 20:52:52 +0100

    wip

--- functional_widget/example/lib/main.dart
+++ functional_widget/example/lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/foundation.dart';
 import 'package:functional_widget_annotation/functional_widget_annotation.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart';
+
 
 part 'main.g.dart';
 
 // we create a widget using a widget decorated by `@widget`
 @widget
 Widget foo(int value) {
+  return Container();
+}
+
+@widget
+Widget example(int foo, String bar) {
   return Container();
 }
 
--- functional_widget/example/lib/main.g.dart
+++ functional_widget/example/lib/main.g.dart
@@ -6,7 +6,7 @@ part of 'main.dart';
 // FunctionalWidgetGenerator
 // **************************************************************************
 
-class Foo extends StatelessWidget {
+class Foo extends HookWidget {
   const Foo(this.value, {Key key}) : super(key: key);
 
   final int value;
@@ -22,5 +22,27 @@ class Foo extends StatelessWidget {
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
     properties.add(IntProperty('value', value));
+  }
+}
+
+class Example extends HookWidget {
+  const Example(this.foo, this.bar, {Key key}) : super(key: key);
+
+  final int foo;
+
+  final String bar;
+
+  @override
+  Widget build(BuildContext _context) => example(foo, bar);
+  @override
+  int get hashCode => hashValues(foo, bar);
+  @override
+  bool operator ==(Object o) =>
+      identical(o, this) || (o is Example && foo == o.foo && bar == o.bar);
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(IntProperty('foo', foo));
+    properties.add(StringProperty('bar', bar));
   }
 }

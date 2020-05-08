e22dbb4aedd41603bbd3ba601c6c09973d9d67ad
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2020-05-08 15:31:33 -0500

    some silly format

--- functional_widget/lib/findBeginToken.dart
+++ functional_widget/lib/findBeginToken.dart
@@ -5,7 +5,7 @@ String findBeginToken(ParameterElement element) {
   final parsedLibrary =
       element.session.getParsedLibraryByElement(element.library);
   final node = parsedLibrary.getElementDeclaration(element).node;
-  final parameter = (node is DefaultFormalParameter) ? node.parameter : node;
+  final parameter = node is DefaultFormalParameter ? node.parameter : node;
   if (parameter is SimpleFormalParameter && parameter.type != null) {
     return parameter.type.beginToken.toString();
   }

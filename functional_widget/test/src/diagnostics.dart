6dc93be204718cff331f74e7d4050ab302fd2d8e
Remi Rousselet <darky12s@gmail.com>
2020-12-13 05:55:42 +0000

    fix CI

--- functional_widget/test/src/diagnostics.dart
+++ functional_widget/test/src/diagnostics.dart
@@ -25,15 +25,7 @@ typedef Typedef<T> = void Function(T);
 Widget typedefTest<T>(Typedef a) => Container();
 
 @swidget
-// ignore: undefined_class
-Widget colorTest(Color a) => Container();
-
-@swidget
 Widget functionTest(void Function() a) => Container();
 
 @swidget
 Widget dynamicTest(dynamic a) => Container();
-
-@swidget
-// ignore: implicit_dynamic_parameter
-Widget inferredTest(a) => Container();
